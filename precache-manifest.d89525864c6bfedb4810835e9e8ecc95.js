7d3c6ab6de978391b5b37a9612c2f771dc0f04f5
Dmytro Kolesnyk <dimakoless@gmail.com>
2019-03-15 17:11:50 +0200

    Updates

---
+++ precache-manifest.d89525864c6bfedb4810835e9e8ecc95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "dcc345c51b5a0452ffd2",
+    "url": "/static/js/main.a416c14d.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "dcc345c51b5a0452ffd2",
+    "url": "/static/css/main.3c2e773e.chunk.css"
+  },
+  {
+    "revision": "a858227704ce6372087fe8936444b7ab",
+    "url": "/index.html"
+  }
+];
